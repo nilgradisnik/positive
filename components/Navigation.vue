9370744d9a060cbddb8479d4dc2c210ef3ad6efc
Nil Gradisnik <nil@layer.com>
2017-08-30 21:53:32 -0700

    Index page updated

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,22 +1,34 @@
 <template>
   <nav>
-    <a href="food">Food</a>
-    <a href="potions">Potions</a>
-    <a href="ethos">Ethos</a>
-    <a href="moments">Moments</a>
-    <a href="order">Order</a>
+    <a class="icn_food" href="food">Food</a>
+    <a class="icn_potions" href="potions">Potions</a>
+    <a class="icn_ethos" href="ethos">Ethos</a>
+    <a class="icn_potions" href="moments">Moments</a>
+    <a class="icn_order" href="order">Order</a>
   </nav>
 </template>
 
 <style lang="stylus" scoped>
+nav-icons = icn_food,
+            icn_potions,
+            icn_ethos,
+            icn_order
 nav
   display: flex
   flex: 1
   position: absolute
   top: 0
   a
-    padding: 34px 6px 0 6px
+    margin: 0 4px
+    padding: 48px 6px 0 6px
     color: white
-    font-size: 14px
+    font-size: 12px
+    font-weight: bold
     text-transform: uppercase
+    background-repeat: no-repeat
+    background-position: center center
+  for icn in nav-icons
+    .{icn}
+      retina-image(icn + '.png')
+
 </style>
--- components/Social.vue
+++ components/Social.vue
@@ -1,19 +1,25 @@
 <template>
   <nav class="social-nav">
-    <a>Twitter</a>
-    <a>Instagram</a>
+    <a class="icn_twitter" href="https://twitter.com" target="_blank"></a>
+    <a class="icn_instagram" href="https://www.instagram.com/positivefoodco/" target="_blank"></a>
   </nav>
 </template>
 
 <style lang="stylus">
+social-icons = icn_twitter, icn_instagram
+
 .social-nav
   display: flex
   flex: 1
   position: absolute
   bottom: 0
   a
-    padding: 0 6px 34px 6px
-    color: white
-    font-size: 14px
-    text-transform: uppercase
+    width: 48px
+    height: 48px
+    margin: 0 10px 16px 10px
+    background-repeat: no-repeat
+    background-position: center center
+  for icn in social-icons
+    .{icn}
+      retina-image(icn + '.png')
 </style>
